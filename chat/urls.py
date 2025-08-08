cebcb09766b0c15e9ad991c6aa9de81f51fb850f
Matheus Zaino Pinto Oliveira <matheuszainopo@gmail.com>
2025-08-08 15:38:10 -0300

    ajeitando umas coisinhas no cloudinary

--- chat/urls.py
+++ chat/urls.py
@@ -6,7 +6,6 @@ app_name = 'chat' # Essencial para o namespace 'chat' funcionar
 
 urlpatterns = [
     path('dm/<str:username>/', views.start_dm_view, name='start-dm'),
-    # ADICIONE ESTA LINHA
     path('<str:room_name>/delete/', views.delete_room_view, name='delete-room'),
 
     path('heartbeat/', views.heartbeat_view, name='heartbeat'),
