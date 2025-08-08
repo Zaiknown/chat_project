75e21b7b1938e29df92a0d5963b596860ba751ff
Matheus Zaino Pinto Oliveira <matheuszainopo@gmail.com>
2025-08-07 21:03:43 -0300

    AJEITANDO O ASGI.PY

--- backend/asgi.py
+++ backend/asgi.py
@@ -1,11 +1,13 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import chat.routing  # IMPORTANTE: Importe o roteamento do seu app de chat
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django.setup()
 
+import chat.routing
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
