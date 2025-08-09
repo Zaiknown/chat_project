9d54971940dbb8eec4cf99e6a72b9c4cead50632
Matheus Zaino Pinto Oliveira <matheuszainopo@gmail.com>
2025-08-09 16:37:36 -0300

    Corrigindo problemas de fotos localhost

--- backend/urls.py
+++ backend/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
 ]
 
 # Configuração para servir arquivos de mídia
-#if settings.DEBUG:
-#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
