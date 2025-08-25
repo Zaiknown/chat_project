28f3ec1f7e6aad662cdc8d0a30620ab3f00891d6
Matheus Zaino Pinto Oliveira <matheuszainopo@gmail.com>
2025-08-25 00:21:24 -0300

    issues with render again

--- backend/urls.py
+++ backend/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from chat import views as chat_views
 from django.contrib.auth import views as auth_views
 from django.conf import settings # ADICIONE ESTA LINHA
 from django.conf.urls.static import static # ADICIONE ESTA LINHA
 from chat.forms import CustomAuthenticationForm
+from django.views.static import serve # ADICIONE esta linha
 
 urlpatterns = [
     path('', chat_views.index_view, name='index'),
@@ -16,6 +17,7 @@ urlpatterns = [
     path('credits/', chat_views.credits_view, name='credits'),
     path('heartbeat/', chat_views.heartbeat_view, name='heartbeat'),
     path('chat/', include('chat.urls', namespace='chat')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 if settings.DEBUG is False:
